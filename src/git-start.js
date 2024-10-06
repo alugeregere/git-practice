1b8063c1c11f5221855526e8f7ae35e96fec3188
Alias <alugeregere@gmail.com>
2024-10-07 00:34:17 +0200

    New Version

--- src/git-start.js
+++ src/git-start.js
@@ -1,7 +1,3 @@
 console.log("git start");
 console.log("This is new version");
-<<<<<<< HEAD
 console.log("Version 12  ");
-=======
-console.log("Version 14");
->>>>>>> 0c6d35aa44d3b585b71b1d71873a86feea45a2a3
